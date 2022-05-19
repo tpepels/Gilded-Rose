1ad404682a97ab0950effb8f23f117545b8b77b6
Tom <tpepels@gmail.com>
2022-05-19 14:06:13 +0200

    Update gitignore

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -3,23 +3,23 @@ package com.gildedrose;
 import com.gildedrose.items.UpdateableItem;
 
 public class TexttestFixture {
+    
     public static void main(String[] args) {
-        System.out.println("OMGHAI!");
 
         ItemFactory itemFactory = new ItemFactory();
-        UpdateableItem item = itemFactory.createItem("+5 Dexterity Vest", 10, 20);
+        UpdateableItem item1 = itemFactory.createItem("+5 Dexterity Vest", 10, 20);
         UpdateableItem item2 = itemFactory.createItem("Aged Brie", 2, 0);
+        UpdateableItem item3 = itemFactory.createItem("Sulfaras", 0, 0);
 
-        GildedRose app = new GildedRose(new Item[] { item, item2 });
+        Item[] items = new Item[]{item1, item2};
+        GildedRose app = new GildedRose(items));
 
-        int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
+        int days = 4;
 
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
+
             for (Item item : items) {
                 System.out.println(item);
             }
